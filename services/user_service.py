a0cbcc8b66fffb2795ab5cbe9f747c26c6e13750
Milton Mesa Manrique <ma.mesam1@uniandes.edu.co>
2025-05-28 08:37:27 -0500

    Revert "cambios a user"
    This reverts commit 4baf3dfbe651aa2de15872fb537647650b7c625b.

--- services/user_service.py
+++ services/user_service.py
@@ -1,4 +1,4 @@
-from repository.user_repo import create_user, get_user_by_emai, get_user_id, get_users, update_user
+from repository.user_repo import create_user, get_user_by_emai, get_user_id, get_users
 from schemas.user import UserCreate
 
 # Funciones de servicio
@@ -6,14 +6,10 @@ def register_user(user: UserCreate):
     return create_user(user)
 
 def get_all_users():
-    return get_users()
+    return get_users()  
 
 def get_user_by_email(email):
     return get_user_by_emai(email)
 
 def get_user_by_id(user_id):
     return get_user_id(user_id)
-
-# Actualizar productos publicados y comprados de un usuario
-def update_user_products(email: str, postedProducts: list, boughtProducts: list):
-    return update_user(email, postedProducts, boughtProducts)
